6605bced3ce9bae91bca5785d73323dfd9bb9195
Sam <sambhatt250@gmail.com>
2020-01-09 03:55:00 +0530

    added test cases

--- src/main/java/com/javaSpark/dataFrameExample/DataFrameOp.java
+++ src/main/java/com/javaSpark/dataFrameExample/DataFrameOp.java
@@ -11,110 +11,104 @@ import static org.apache.spark.sql.functions.*;
 
 public class DataFrameOp {
 
- private static Dataset<Row> createDataFrame(SparkSession spark,String csvPath) throws AnalysisException {
-	    /*
-	    This is to create the dataframe with a csv file
-	    */
-	    Dataset<Row> df = spark.read().option("header", "true").csv(csvPath);
-	    return df;
-	}
-	
- private static void findSalesAndBroadcastRights(Dataset<Row> whatsonDF, Dataset<Row> startedStreamsDF) throws AnalysisException {
-	 
-	 /*
-	 This function matches the whatson and streams on basis of house number to find the broadcast rights for each house 
-	 */
-	 
-	 //This dataframe finds the latest date in whatson
-	 Dataset<Row> whatsonMaxDtDF=whatsonDF.groupBy("house_number").agg(max("dt").as("dt"))
-			                   .withColumnRenamed("house_number", "house_number_whatson").withColumnRenamed("dt", "dt_whatson")
-			                   .join(whatsonDF, col("house_number_whatson").equalTo(whatsonDF.col("house_number"))
-			                		   .and(col("dt_whatson").equalTo(whatsonDF.col("dt")))).drop("house_number").drop("dt");
-	 //whatsonMaxDtDF.show();
-	 
-	 //This dataframe is the final dataframe where it matches with the streams data on basis of house number
-	 //Filters product types as per the given criteria
-	 Dataset<Row> broadcastRightsDF = startedStreamsDF.filter("product_type = 'tvod' OR product_type = 'est'")
-			                          .join(whatsonMaxDtDF, startedStreamsDF.col("house_number").equalTo(whatsonMaxDtDF.col("house_number_whatson")))
-			                          .select("dt_whatson","time","device_name","house_number","user_id","country_code","program_title","season","season_episode","genre","product_type","broadcast_right_start_date","broadcast_right_end_date")
-			                          .withColumnRenamed("dt_whatson", "dt")
-			                          ;
-	 System.out.println( "1.	Sales and rentals broadcast rights results======================================>" );
-	 
-	 //Printing sample output
-	 broadcastRightsDF.show(100,false);
- }
- 
- private static void findProductAndUserCount(Dataset<Row> startedStreamsDF) throws AnalysisException {
-	 //Finding the unique user count for each program
-	 Dataset<Row> UserCountDF=startedStreamsDF.dropDuplicates("program_title","user_id").groupBy("program_title").agg(count("user_id").as("unique_users")).cache()
-			                          ;
-	 
-	 //Finding the total count of watches for each program
-	 Dataset<Row> ProductAndUserCountDF=startedStreamsDF.groupBy("program_title").agg(count("user_id").as("content_count"))
-			                           .join(startedStreamsDF,"program_title").dropDuplicates("program_title")			                           
-			                           .select("dt","program_title","device_name","country_code","product_type","content_count")
-			                           ;
-	 System.out.println( "2.	Product and user count results======================================>" );
-	 
-	 //Final dataframe where it displays all the results together
-	 UserCountDF.join(ProductAndUserCountDF,"program_title").select("dt","program_title","device_name","country_code","product_type","unique_users","content_count").show(100,false); 
-	 
- }
- 
- private static void findGenreAndTimeOfDay(Dataset<Row> startedStreamsDF) throws AnalysisException {
-	 //Finding unique users per genre
-	 Dataset<Row> GenreUniqUserDF=startedStreamsDF.dropDuplicates("genre","user_id").groupBy("genre").agg(count("user_id").as("unique_users")).cache();
-	 
-	 //Extracting average hour which will identify the an approx hour in a day to estimate which hour each genre is watched most
-	 Dataset<Row> GenreHourDF=startedStreamsDF.withColumn("watched_hour",  from_unixtime(unix_timestamp(col("time"), "HH:mm:ss"), "HH"))
-	                .groupBy("genre")
-	                .agg(round(avg("watched_hour")).as("watched_hour"))
-	                .dropDuplicates("genre");
-	
-	 System.out.println( "3.	Genre and time of day results======================================>" );
-	 
-	 //Final dataframe to display the end result together
-	 GenreUniqUserDF.join(GenreHourDF,"genre")
-	                .select("watched_hour","genre","unique_users").sort(desc("unique_users")).show(false);
-	 
-	 
- }
- 
- public static void main(String[] args) throws AnalysisException {
-	   
-	    SparkSession spark = SparkSession
-	      .builder()
-	      .appName("DFAssignment")
-	      .master("local")
-	      .getOrCreate();
-	    
-	    spark.sparkContext().setLogLevel("ERROR");
+ public static Dataset<Row> createDataFrame(SparkSession spark,String csvPath) throws AnalysisException {
+            /*
+            This is to create the dataframe with a csv file
+            */
+            Dataset<Row> df = spark.read().option("header", "true").csv(csvPath);
+            return df;
+        }
 
-	    
+ public static Dataset<Row> findSalesAndBroadcastRights(Dataset<Row> whatsonDF, Dataset<Row> startedStreamsDF) throws AnalysisException {
+
+         /*
+         This function matches the whatson and streams on basis of house number to find the broadcast rights for each house
+         */
+
+         //This dataframe finds the latest date in whatson
+         Dataset<Row> whatsonMaxDtDF=whatsonDF.groupBy("house_number").agg(max("dt").as("dt"))
+                                           .withColumnRenamed("house_number", "house_number_whatson").withColumnRenamed("dt", "dt_whatson")
+                                           .join(whatsonDF, col("house_number_whatson").equalTo(whatsonDF.col("house_number"))
+                                                           .and(col("dt_whatson").equalTo(whatsonDF.col("dt")))).drop("house_number").drop("dt");
+         //whatsonMaxDtDF.show();
+
+         //This dataframe is the final dataframe where it matches with the streams data on basis of house number
+         //Filters product types as per the given criteria
+         Dataset<Row> broadcastRightsDF = startedStreamsDF.filter("product_type = 'tvod' OR product_type = 'est'")
+                                                  .join(whatsonMaxDtDF, startedStreamsDF.col("house_number").equalTo(whatsonMaxDtDF.col("house_number_whatson")))
+                                                  .select("dt_whatson","time","device_name","house_number","user_id","country_code","program_title","season","season_episode","genre","product_type","broadcast_right_start_date","broadcast_right_end_date")
+                                                  .withColumnRenamed("dt_whatson", "dt")
+                                                  ;
+         
+		 return broadcastRightsDF;
+ }
+
+ public static Dataset<Row> findProductAndUserCount(Dataset<Row> startedStreamsDF) throws AnalysisException {
+         //Finding the unique user count for each program
+         Dataset<Row> UserCountDF=startedStreamsDF.dropDuplicates("program_title","user_id").groupBy("program_title").agg(count("user_id").as("unique_users")).cache()
+                                                  ;
+
+         //Finding the total count of watches for each program
+         Dataset<Row> ProductAndUserCountDF=startedStreamsDF.groupBy("program_title").agg(count("user_id").as("content_count"))
+                                                   .join(startedStreamsDF,"program_title").dropDuplicates("program_title")
+                                                   .select("dt","program_title","device_name","country_code","product_type","content_count")
+                                                   ;
+         
+		 return UserCountDF.join(ProductAndUserCountDF,"program_title").select("dt","program_title","device_name","country_code","product_type","unique_users","content_count"); 
+ }
+
+ public static Dataset<Row> findGenreAndTimeOfDay(Dataset<Row> startedStreamsDF) throws AnalysisException {
+         //Finding unique users per genre
+         Dataset<Row> GenreUniqUserDF=startedStreamsDF.dropDuplicates("genre","user_id").groupBy("genre").agg(count("user_id").as("unique_users")).cache();
+
+         //Extracting average hour which will identify the an approx hour in a day to estimate which hour each genre is watched most
+         Dataset<Row> GenreHourDF=startedStreamsDF.withColumn("watched_hour",  from_unixtime(unix_timestamp(col("time"), "HH:mm:ss"), "HH"))
+                        .groupBy("genre")
+                        .agg(round(avg("watched_hour")).as("watched_hour"))
+                        .dropDuplicates("genre");
+
+        return GenreUniqUserDF.join(GenreHourDF,"genre")
+	                .select("watched_hour","genre","unique_users").sort(desc("unique_users"));
+
+
+ }
+
+ public static void main(String[] args) throws AnalysisException {
+
+            SparkSession spark = SparkSession
+              .builder()
+              .appName("DFAssignment")
+              .master("local")
+              .getOrCreate();
+
+            spark.sparkContext().setLogLevel("ERROR");
+
+
         //String whatsonCSVPath="C:\\Users\\Sam\\Desktop\\assignments\\whatson.csv";
-	    String whatsonCSVPath= args[0];
+            String whatsonCSVPath= args[0];
         //String streamsCSVPath="C:\\Users\\Sam\\Desktop\\assignments\\started_streams_new.csv";
-	    String streamsCSVPath = args[1];
-        
+            String streamsCSVPath = args[1];
+
         Dataset<Row> whatsonDF=createDataFrame(spark,whatsonCSVPath);
         Dataset<Row> streamsDF=createDataFrame(spark,streamsCSVPath);
-        
+
         System.out.println( "WhatsOn sample data======================================>" );
         whatsonDF.show(false);
-        
+
         System.out.println( "Streams sample data======================================>" );
         streamsDF.show(false);
         
-       
-        findSalesAndBroadcastRights(whatsonDF,streamsDF);
+		System.out.println( "1.	Sales and rentals broadcast rights results======================================>" );
+        findSalesAndBroadcastRights(whatsonDF,streamsDF).show(100,false);
         
-        findProductAndUserCount(streamsDF);
+        System.out.println( "2.	Product and user count results======================================>" );
+        findProductAndUserCount(streamsDF).show(100,false);
 
-        findGenreAndTimeOfDay(streamsDF);
-        
-	    spark.stop();
-	  }	
-	
-	
+        System.out.println( "3.	Genre and time of day results======================================>" );
+        findGenreAndTimeOfDay(streamsDF).show();
+
+            spark.stop();
+          }
+
+
 }
--- src/test/java/com/javaSpark/dataFrameExample/DataFrameOpTest.java
+++ src/test/java/com/javaSpark/dataFrameExample/DataFrameOpTest.java
@@ -4,6 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+
+import org.apache.spark.sql.AnalysisException;
+
 public class DataFrameOpTest 
     extends TestCase
 {
@@ -24,12 +31,71 @@ public class DataFrameOpTest
     {
         return new TestSuite( DataFrameOpTest.class );
     }
+   
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
+        {
+	        assertTrue( true );
+	}
+
+
+    SparkSession spark = SparkSession
+  	      .builder()
+  	      .appName("DFAssignment")
+  	      .master("local")
+  	      .getOrCreate();
+    
+    String whatsOnFilePath=System.getProperty("user.dir") + "/files/whatsonSample.csv";
+    String streamsFilePath=System.getProperty("user.dir") + "/files/streamsSample.csv";
+    
+    
+    /**
+     * @return Test the createDataFrame function
+     */
+    
+    public void testAppcreateDataFrame() throws AnalysisException
     {
-        assertTrue( true );
+    	        
+    	Dataset<Row> whatsonDF= DataFrameOp.createDataFrame(spark, whatsOnFilePath);
+    	
+    	assertTrue( whatsonDF.count() >= 0 );
     }
+    
+    /**
+     * @return Test the findSalesAndBroadcastRights function
+     */
+    
+    public void testAppfindSalesAndBroadcastRights() throws AnalysisException
+    {
+    	        
+    	Dataset<Row> whatsonDF= DataFrameOp.createDataFrame(spark, whatsOnFilePath);
+    	Dataset<Row> streamsDF=DataFrameOp.createDataFrame(spark,streamsFilePath);
+    	   	
+    	assertTrue( DataFrameOp.findSalesAndBroadcastRights(whatsonDF,streamsDF).count() >= 0 );
+    }  
+   
+    /**
+     * @return Test the findProductAndUserCount function
+     */
+    
+    public void testAppfindProductAndUserCount() throws AnalysisException
+    {
+    	        
+    	Dataset<Row> streamsDF=DataFrameOp.createDataFrame(spark,streamsFilePath);
+    	   	
+    	assertTrue( DataFrameOp.findProductAndUserCount(streamsDF).count() >= 0 );
+    }
+    
+    /**
+     * @return Test the findGenreAndTimeOfDay function
+     */
+    
+    public void testAppfindGenreAndTimeOfDay() throws AnalysisException
+    {
+    	        
+    	Dataset<Row> streamsDF=DataFrameOp.createDataFrame(spark,streamsFilePath);
+    	   	
+    	assertTrue( DataFrameOp.findGenreAndTimeOfDay(streamsDF).count() >= 0 );
+    }
+    
 }
